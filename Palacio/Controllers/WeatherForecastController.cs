a9cecd40e73d69378a427d8658fff08d1c0eae0f
Omar Meneses <fernandomenesesda@gmail.com>
2024-08-21 20:24:06 -0500

    m

--- Palacio/Controllers/WeatherForecastController.cs
+++ Palacio/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace Palacio.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Time"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Times"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
