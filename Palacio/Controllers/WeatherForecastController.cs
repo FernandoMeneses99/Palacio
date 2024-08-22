ecb29c3210a2f6f3bb99ab4ca93fdf169ecb22fc
Omar Meneses <fernandomenesesda@gmail.com>
2024-08-21 19:45:09 -0500

    Se sube un cambio prueba

--- Palacio/Controllers/WeatherForecastController.cs
+++ Palacio/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace Palacio.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Time"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
