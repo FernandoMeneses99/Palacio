224d01dc67a1e6d4791bdce65b9a24ef99c5435b
Omar Meneses <fernandomenesesda@gmail.com>
2024-08-23 20:21:48 -0500

    Se crea el controlador del login

---
+++ Palacio/Controllers/LoginController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Palacio.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+    }
+}
